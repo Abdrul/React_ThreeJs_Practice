eff1c613719b4442d9bcc2c2e1ced95d7655f7f4
Abel <josephine.abel@outlook.fr>
2022-10-27 19:05:15 +0200

    add experience file

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -4,9 +4,16 @@ export default function Experience() {
   return (
     <>
       <mesh position-x={2} scale={1.5} rotation-y={Math.PI * 0.25}>
-        {/* <sphereGeometry args={[1.5, 32, 32]} /> */}
         <boxGeometry />
-        <meshBasicMaterial color="black" wireframe />
+        <meshBasicMaterial color="mediumpurple" />
+      </mesh>
+      <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
+        <planeGeometry />
+        <meshBasicMaterial color="greenyellow" />
+      </mesh>
+      <mesh position-x={-2}>
+        <sphereGeometry />
+        <meshBasicMaterial color="orange" />
       </mesh>
     </>
   );
