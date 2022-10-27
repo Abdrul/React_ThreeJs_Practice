2ab00627ab05ef69e1ebd877fa952007a1fe54ef
Abel <josephine.abel@outlook.fr>
2022-10-27 19:42:58 +0200

    add orbicontrol and animate mesh

--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,19 +1,38 @@
-import React from "react";
+import React, { useRef } from "react";
+import { extend, useFrame, useThree } from "@react-three/fiber";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
+extend({ OrbitControls });
 
 export default function Experience() {
+  const { camera, gl } = useThree();
+  const cubeRef = useRef();
+
+  useFrame((state, delta) => {
+    cubeRef.current.rotation.y += delta;
+  });
+
   return (
     <>
-      <mesh position-x={2} scale={1.5} rotation-y={Math.PI * 0.25}>
-        <boxGeometry />
-        <meshBasicMaterial color="mediumpurple" />
-      </mesh>
+      <orbitControls args={[camera, gl.domElement]} />
+      <group>
+        <mesh
+          ref={cubeRef}
+          position-x={2}
+          scale={1.5}
+          rotation-y={Math.PI * 0.25}
+        >
+          <boxGeometry />
+          <meshBasicMaterial color="mediumpurple" />
+        </mesh>
+        <mesh position-x={-2}>
+          <sphereGeometry />
+          <meshBasicMaterial color="orange" />
+        </mesh>
+      </group>
       <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
         <planeGeometry />
         <meshBasicMaterial color="greenyellow" />
-      </mesh>
-      <mesh position-x={-2}>
-        <sphereGeometry />
-        <meshBasicMaterial color="orange" />
       </mesh>
     </>
   );
