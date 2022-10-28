9888ceeabb4c6cd6cb7baf8e03c973e0885463a4
Abel <josephine.abel@outlook.fr>
2022-10-28 13:27:11 +0200

    end of lesson general use R3F

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import { Canvas } from "@react-three/fiber";
 import Experience from "./Experience";
 import "./App.css";
+import * as THREE from "three";
 
 function App() {
   return (
     <div className="App">
-      <Canvas>
+      <Canvas
+        // dpr={1}
+        gl={{
+          antialias: true,
+          toneMapping: THREE.ACESFilmicToneMapping,
+          outputEncoding: THREE.sRGBEncoding,
+        }}
+        camera={{
+          fov: 45,
+          near: 0.1,
+          far: 200,
+          position: [7, 5, 9],
+        }}
+      >
         <Experience />
       </Canvas>
     </div>
--- src/Experience.jsx
+++ src/Experience.jsx
@@ -1,6 +1,8 @@
 import React, { useRef } from "react";
 import { extend, useFrame, useThree } from "@react-three/fiber";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import CustomObject from "./CustomObject";
+import * as THREE from "three";
 
 extend({ OrbitControls });
 
@@ -9,12 +11,23 @@ export default function Experience() {
   const cubeRef = useRef();
 
   useFrame((state, delta) => {
+    // const angle = state.clock.elapsedTime;
+
+    // state.camera.position.x = Math.sin(angle) * 8;
+    // state.camera.position.z = Math.cos(angle) * 8;
+
+    // state.camera.lookAt(0, 0, 0);
+
     cubeRef.current.rotation.y += delta;
   });
 
   return (
     <>
       <orbitControls args={[camera, gl.domElement]} />
+
+      <directionalLight position={[1, 2, 3]} intensity={1.5} />
+      <ambientLight intensity={0.5} />
+
       <group>
         <mesh
           ref={cubeRef}
@@ -23,17 +36,19 @@ export default function Experience() {
           rotation-y={Math.PI * 0.25}
         >
           <boxGeometry />
-          <meshBasicMaterial color="mediumpurple" />
+          <meshStandardMaterial color="mediumpurple" />
         </mesh>
         <mesh position-x={-2}>
           <sphereGeometry />
-          <meshBasicMaterial color="orange" />
+          <meshStandardMaterial color="orange" />
         </mesh>
       </group>
       <mesh rotation-x={-Math.PI * 0.5} position-y={-1} scale={10}>
         <planeGeometry />
-        <meshBasicMaterial color="greenyellow" />
+        <meshStandardMaterial color="greenyellow" side={THREE.DoubleSide} />
       </mesh>
+
+      <CustomObject />
     </>
   );
 }
